7613a8e40745ebfca17fb81dd6c8851b13fec597
Shruthaja <mail.shruthaja@gmail.com>
2023-06-15 15:59:34 -0500

    first task

--- app.py
+++ app.py
@@ -13,10 +13,24 @@ driver = '{ODBC Driver 17 for SQL Server}'
 conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
 
 cursor = conn.cursor()
-@app.route('/', methods=['GET', 'POST'])
 
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return render_template("index.html")
+    result = []
+    if request.method == "POST":
+        cname = request.form['cityname']
+        query = "select * from dbo.city where City=?"
+        cursor.execute(query, cname)
+        result = cursor.fetchall()
+        query = " select lat , lon from dbo.city where City=? "
+        cursor.execute(query, cname)
+        lr = cursor.fetchone()
+        query = "SELECT * FROM [dbo].[city] WHERE ( 6371 * ACOS(COS(RADIANS(lat)) * COS(RADIANS(?)) * COS(RADIANS(lon) - RADIANS(?)) + SIN(RADIANS(lat)) * SIN(RADIANS(?)) ))< ?;"
+        cursor.execute(query, lr[0], lr[1], lr[0], 100)
+        r = cursor.fetchall()
+    return render_template("index.html", result=result, r=r)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
